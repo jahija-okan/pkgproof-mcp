f1655460bbcfe20cf05bba3de887152ad06cda0a
Jahija Okan <>
2026-09-08 13:46:56 +0200

    trusted publishing update

--- src/meta.ts
+++ src/meta.ts
@@ -11,7 +11,7 @@
  *  `mcpName` in package.json and as `name` in server.json. */
 export const SERVER_NAME = "pkgproof";
 
-export const SERVER_VERSION = "0.1.0";
+export const SERVER_VERSION = "0.1.1";
 
 export type RailId = "algorand" | "base";
 
